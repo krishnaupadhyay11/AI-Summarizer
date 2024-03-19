b995135d110964fb4981145ca88ad0057a17e976
Krishna Upadhyay <krishna.upadhyay911@gmail.com>
2024-03-19 17:57:13 +0530

    keepgo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        santoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
