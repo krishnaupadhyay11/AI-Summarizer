4aac690d79862f8ea9c2c7e8ef05977e3640c16a
Krishna Upadhyay <krishna.upadhyay911@gmail.com>
2024-04-13 20:18:10 +0530

    keepgo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,14 +3,25 @@ import { useState, useEffect } from "react";
 import {copy, linkIcon, loader, tick} from '../assets';
 
 export default function Demo() {
+    const [article, setArticle] = useState({
+        url: '',
+        summary: ''
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        alert('Submitted');
+        setArticle({...article, url:''})
+    }
+
   return (
     <section className="w-full mt-16 max-w-xl">
         {/* Search */}
         <div className="w-full flex flex-col gap-2">
-            <form className="relative flex justify-center items-center" onSubmit={() => {}}>
+            <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
                 <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
                 
-                <input type="url" placeholder="Enter a value" value="" onChange={() => {}} required className="url_input peer" />
+                <input type="url" placeholder="Enter a value" value={article.url} onChange={(e) => setArticle({...article, url: e.target.value})} required className="url_input peer" />
 
                 <button
                     type="submit"
@@ -25,7 +36,7 @@ export default function Demo() {
         </div>
 
         {/* Display Result */}
-        
+
     </section>
   )
 }
